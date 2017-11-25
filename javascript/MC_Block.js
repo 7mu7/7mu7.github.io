80a45e283b7581e6430250e9ab81e24443164d1e
Billy <30966704+trillman111@users.noreply.github.com>
2017-11-25 18:58:48 +0000

    Update MC_Block.js

--- javascript/MC_Block.js
+++ javascript/MC_Block.js
@@ -1,4 +1,4 @@
 loadImage2 = new Image();
 loadImage2.src = "images/minecraft-blockmoving.gif";
 staticImage2 = new Image();
-staticImage2.src = "7mu7.github.io/images/minecraft-block/minecraft-block1.gif";
+staticImage2.src = "7mu7.github.io/images/minecraft-block/minecraft-block3.gif";
