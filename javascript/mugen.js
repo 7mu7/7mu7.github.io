f8068da37d235d83ee69232cbfc0e12359a6f2c1
7mu7 <peter95buntyn@gmail.com>
2018-05-03 17:32:02 -0400

    s

--- javascript/mugen.js
+++ javascript/mugen.js
@@ -1,7 +1,7 @@
 var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {
-          dosbox.run("../arcade/mugen/mugen.exe");
+          dosbox.run("../arcade/mugen.zip", "./mugen.exe");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is runned");
