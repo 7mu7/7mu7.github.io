5374e8a4f3f2dc8a39a05e7512b2dd715f89d728
7mu7 <peter95buntyn@gmail.com>
2018-05-22 00:27:26 -0400

    update chat

---
+++ javascript/servermenu.js
@@ -0,0 +1,8 @@
+loadImage1 = new Image();
+loadImage1.src = "images/home2.png";
+staticImage1 = new Image();
+staticImage1.src = "images/home1.png";
+loadImage2 = new Image();
+loadImage2.src = "images/chat.gif";
+staticImage2 = new Image();
+staticImage2.src = "images/chatstill.png";
