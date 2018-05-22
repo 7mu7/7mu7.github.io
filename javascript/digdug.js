20cfe1a01c864a4d69d11b7ed73a5110901475ab
7mu7 <peter95buntyn@gmail.com>
2018-05-22 01:32:41 -0400

    com

--- javascript/digdug.js
+++ javascript/digdug.js
@@ -1,7 +1,7 @@
 var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {
-          dosbox.run("../arcade/digdug.zip", "./dig-dug/digdug.exe");
+          dosbox.run("../arcade/digdug.zip", "./dig-dug/digdug.com");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is runned");
