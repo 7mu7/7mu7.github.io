3bf2b4ddb5623e2e74897cfd33be0fb3362ad22b
7mu7 <peter95buntyn@gmail.com>
2018-05-23 23:39:06 -0400

    test

--- javascript/oregontrail.js
+++ javascript/oregontrail.js
@@ -1,7 +1,7 @@
 var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {
-          dosbox.run("../arcade/warcraft-ii-tides-of-darkness.zip", "./warcraft-ii-tides-of-darkness/war2.exe");
+          dosbox.run("../arcade/WARCRAFT1.zip", "./WARCRAFT1/WAR.EXE");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is runned");
