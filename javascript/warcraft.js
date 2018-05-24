785c127b8126f98cb97811fa598112d17e694e93
7mu7 <peter95buntyn@gmail.com>
2018-05-24 01:14:56 -0400

    change

--- javascript/warcraft.js
+++
@@ -1,9 +0,0 @@
-var dosbox = new Dosbox({
-        id: "dosbox",
-        onload: function (dosbox) {
-          dosbox.run("../arcade/warcraft.zip", "./WARCRAFT1/WAR.EXE");
-        },
-        onrun: function (dosbox, app) {
-          console.log("App '" + app + "' is runned");
-        }
-      });
