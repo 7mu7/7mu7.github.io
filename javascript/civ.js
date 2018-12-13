56d92d08e5bd12afc103a143cd39a76806d409c4
7mu7 <peter95buntyn@gmail.com>
2018-12-13 16:20:17 -0500

    url

--- javascript/civ.js
+++ javascript/civ.js
@@ -1,7 +1,7 @@
 var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {
-          dosbox.run("../arcade/Sid-Meiers-Civilization_DOS_EN.zip", "./civ/CIV.EXE");
+          dosbox.run("../arcade/civ.zip", "./civ/CIV.EXE");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is runned");
