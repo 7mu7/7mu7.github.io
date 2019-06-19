2b0b271986a2ef217a53d5575556b748a42fa06b
7mu7 <peter95buntyn@gmail.com>
2019-06-18 22:12:33 -0400

    z

--- javascript/tetris.js
+++ javascript/tetris.js
@@ -1,7 +1,7 @@
 var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {
-          dosbox.run("../arcade/tetris.zip", "./tetris.com");
+          dosbox.run("../arcade/tetris.zip", "./tinytris.com");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is runned");
