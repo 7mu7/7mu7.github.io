ecb9195c3e7d9a7a7a6730fa386babdb4eca725c
7mu7 <peter95buntyn@gmail.com>
2019-10-14 20:23:24 -0400

    smash table
    need compile

---
+++ 3dmallUnity/Assets/smash.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smash : MonoBehaviour {
+
+	void PlayerHit(ControllerColliderHit hit){
+		Application.OpenURL("smash.htm");
+	}
+}
