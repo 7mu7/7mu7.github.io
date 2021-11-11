7ec3f7a47ba134741d51397bb1dd7963a66582d3
7mu7 <peter95buntyn@gmail.com>
2021-11-11 15:46:25 -0500

    rt

--- javascript/doom2.js
+++ javascript/doom2.js
@@ -1,7 +1,7 @@
 var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {
-          dosbox.run("../arcade/doom2.zip", "./doomiido/DOOM2.EXE");
+          dosbox.run("../arcade/doom2.zip", "./doomiido/DOOM2.EXE" -file "./met2.pk3");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is runned");
