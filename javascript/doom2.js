6d298ae59214a2d8d1119d2820652fc49a9a0419
7mu7 <peter95buntyn@gmail.com>
2022-01-25 01:05:49 -0500

    game

--- javascript/doom2.js
+++
@@ -1,9 +0,0 @@
-var dosbox = new Dosbox({
-        id: "dosbox",
-        onload: function (dosbox) {
-          dosbox.run("../arcade/doom2.zip", "./doomiido/DOOM2.EXE" -file "./met2.pk3");
-        },
-        onrun: function (dosbox, app) {
-          console.log("App '" + app + "' is runned");
-        }
-      });
