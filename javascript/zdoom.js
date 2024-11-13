88c1403d806764085b83e6d805dae754bd260d4f
7mu7 <itsme@7mu7.dev>
2024-11-13 18:03:12 -0500

    zdoom

--- javascript/zdoom.js
+++ javascript/zdoom.js
@@ -1,7 +1,7 @@
 var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {
-          dosbox.run("../arcade/zdoom.zip", ".zdoom/zdoomdos.exe");
+          dosbox.run("../arcade/zdoom.zip", "zdoomdos.exe");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is runned");
